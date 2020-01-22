d2893d213d0d982344169e96bad89259d26e6c16
SoyBison <coen@uchicago.edu>
2020-01-22 16:07:58 -0600

    docs

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
+with open('docs/requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='ongaku',
     version='1.0',
     packages=[''],
+    install_requires=requirements,
     url='https://ongaku.readthedocs.io',
     license='',
     author='Coen D. Needell',
