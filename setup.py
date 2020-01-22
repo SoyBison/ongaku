015803e00f53b55c4fed7284806dc54018962497
SoyBison <coen@uchicago.edu>
2020-01-22 15:58:22 -0600

    docs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ongaku',
+    version='1.0',
+    packages=[''],
+    url='https://ongaku.readthedocs.io',
+    license='',
+    author='Coen D. Needell',
+    author_email='',
+    description=''
+)
